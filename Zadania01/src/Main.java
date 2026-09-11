615afc350a2134d410b4708181d8eec8196b6230
Eryk Warwas <erykos4@hotmail.com>
2026-09-11 11:26:53 +0200

    zad3

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -16,5 +16,18 @@ void main() {
 
         }
     }
-    
+
+    //zad3
+    public class Zad3 {
+        public static void main(String[] args) {
+
+            String imie = "Eryk";
+            int rokUrodzenia = 2008;
+            double liczba = 0.66;
+
+            System.out.println("Mam na imię " + imie + ", mam " + (2026 - rokUrodzenia) + " lat i będę pisać maturę za " + liczba + " roku.");
+
+        }
+    }
+
 }
