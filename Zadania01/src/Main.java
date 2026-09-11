7acaef4d1c8360a6a0072550bd4403ebb16d7d14
Eryk Warwas <erykos4@hotmail.com>
2026-09-11 11:51:46 +0200

    zad7

--- Zadania01/src/Main.java
+++ Zadania01/src/Main.java
@@ -93,5 +93,20 @@ void main() {
 
         }
     }
+    //zad7
+    import java.util.Scanner;
+
+    public class Zad7 {
+        public static void main(String[] args) {
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("Podaj wyraz:");
+            String wyraz = scanner.nextLine();
+
+            System.out.println(wyraz.length());
+
+        }
+    }
 
 }
